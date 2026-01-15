f9d264223268b3f98ce13721290f18a711fcce41
proshanto7 <proshantosarker112@gmail.com>
2026-01-16 02:46:10 +0600

    header part responsive done

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -4,12 +4,15 @@ import Profile from "../profile/Profile";
 import { CgProfile } from "react-icons/cg";
 import { useState } from "react";
 
+import { RiCloseCircleLine, RiMenu3Line } from "react-icons/ri";
+
 function Header() {
   const user = useSelector((state) => state.user.value);
   const [profileShow, setProfileShow] = useState(false);
+  const [mobileMenu, setMobileMenu] = useState(false);
 
   return (
-    <header className="flex shadow-md py-4 px-4 sm:px-10 bg-white min-h-17.5 tracking-wide relative z-50">
+    <header className="flex shadow-md py-4  bg-white min-h-17.5 tracking-wide relative z-50">
       <div className="flex flex-wrap items-center justify-between gap-5 w-full">
         <Link
           to="/"
@@ -19,7 +22,7 @@ function Header() {
         </Link>
         <Link
           to="/"
-          className="hidden max-sm:block text-3xl font-bold capitalize "
+          className="hidden max-sm:block text-3xl font-bold capitalize font-serif "
         >
           Proshanto
         </Link>
@@ -27,25 +30,6 @@ function Header() {
           id="collapseMenu"
           className="max-lg:hidden lg:block max-lg:before:fixed max-lg:before:bg-black max-lg:before:opacity-50 max-lg:before:inset-0 max-lg:before:z-50"
         >
-          <button
-            id="toggleClose"
-            className="lg:hidden fixed top-2 right-4 z-100 rounded-full bg-white w-9 h-9 flex items-center justify-center border border-gray-200 cursor-pointer"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-3.5 h-3.5 fill-black"
-              viewBox="0 0 320.591 320.591"
-            >
-              <path
-                d="M30.391 318.583a30.37 30.37 0 0 1-21.56-7.288c-11.774-11.844-11.774-30.973 0-42.817L266.643 10.665c12.246-11.459 31.462-10.822 42.921 1.424 10.362 11.074 10.966 28.095 1.414 39.875L51.647 311.295a30.366 30.366 0 0 1-21.256 7.288z"
-                data-original="#000000"
-              />
-              <path
-                d="M287.9 318.583a30.37 30.37 0 0 1-21.257-8.806L8.83 51.963C-2.078 39.225-.595 20.055 12.143 9.146c11.369-9.736 28.136-9.736 39.504 0l259.331 257.813c12.243 11.462 12.876 30.679 1.414 42.922-.456.487-.927.958-1.414 1.414a30.368 30.368 0 0 1-23.078 7.288z"
-                data-original="#000000"
-              />
-            </svg>
-          </button>
           <ul className="lg:flex gap-x-4 max-lg:space-y-3 max-lg:fixed max-lg:bg-white max-lg:w-1/2 max-lg:min-w-75 max-lg:top-0 max-lg:left-0 max-lg:p-6 max-lg:h-full max-lg:shadow-md max-lg:overflow-auto z-50">
             <li className="mb-6 hidden max-lg:block">
               <a href="javascript:void(0)">
@@ -104,13 +88,23 @@ function Header() {
                 Contact
               </Link>
             </li>
+            <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3">
+              <button className="sm:hidden block px-4 py-2 text-sm rounded-full font-medium cursor-pointer tracking-wide text-slate-900 border border-gray-400 bg-transparent hover:bg-gray-50 transition-all">
+                <Link to="/signin">Login</Link>
+              </button>
+            </li>
+            <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3">
+              <button className="sm:hidden block  px-4 py-2 text-sm rounded-full font-medium cursor-pointer tracking-wide text-white border border-blue-600 bg-blue-600 hover:bg-blue-700 transition-all">
+                <Link to="/singup">Sign up</Link>
+              </button>
+            </li>
           </ul>
         </div>
         <div className="flex max-lg:ml-auto space-x-4">
-          <button className="px-4 py-2 text-sm rounded-full font-medium cursor-pointer tracking-wide text-slate-900 border border-gray-400 bg-transparent hover:bg-gray-50 transition-all">
+          <button className="hidden sm:block px-4 py-2 text-sm rounded-full font-medium cursor-pointer tracking-wide text-slate-900 border border-gray-400 bg-transparent hover:bg-gray-50 transition-all">
             <Link to="/signin">Login</Link>
           </button>
-          <button className="px-4 py-2 text-sm rounded-full font-medium cursor-pointer tracking-wide text-white border border-blue-600 bg-blue-600 hover:bg-blue-700 transition-all">
+          <button className="hidden sm:block  px-4 py-2 text-sm rounded-full font-medium cursor-pointer tracking-wide text-white border border-blue-600 bg-blue-600 hover:bg-blue-700 transition-all">
             <Link to="/singup">Sign up</Link>
           </button>
 
@@ -122,24 +116,148 @@ function Header() {
             </button>
           )}
 
-          <button id="toggleOpen" className="lg:hidden cursor-pointer">
-            <svg
-              className="w-7 h-7"
-              fill="#000"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clipRule="evenodd"
-              />
-            </svg>
+          <button
+            onClick={() => setMobileMenu(!mobileMenu)}
+            className="lg:hidden cursor-pointer"
+          >
+            {mobileMenu ? (
+              <RiCloseCircleLine className="text-3xl" />
+            ) : (
+              <RiMenu3Line className="text-3xl" />
+            )}
           </button>
         </div>
       </div>
 
       {profileShow && <Profile />}
+
+      {mobileMenu ? (
+        <div className="absolute z-999 w-full top-17 translate-x-0 duration-300 block lg:hidden">
+          <ul className="bg-white w-full ">
+            <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3">
+              <a
+                href="javascript:void(0)"
+                className="hover:text-blue-700 text-blue-700 block font-medium text-[15px]"
+              >
+                Home
+              </a>
+            </li>
+            <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3">
+              <Link
+                to="/todolist"
+                className="hover:text-blue-700 text-slate-900 block font-medium text-[15px]"
+              >
+                Todo List
+              </Link>
+            </li>
+            <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3">
+              <Link
+                to="/todo"
+                className="hover:text-blue-700 text-slate-900 block font-medium text-[15px]"
+              >
+                TO-DO
+              </Link>
+            </li>
+            <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3">
+              <a
+                href="javascript:void(0)"
+                className="hover:text-blue-700 text-slate-900 block font-medium text-[15px]"
+              >
+                Blog
+              </a>
+            </li>
+            <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3">
+              <a
+                href="javascript:void(0)"
+                className="hover:text-blue-700 text-slate-900 block font-medium text-[15px]"
+              >
+                About
+              </a>
+            </li>
+            <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3">
+              <Link
+                to="/contact"
+                className="hover:text-blue-700 text-slate-900 block font-medium text-[15px]"
+              >
+                Contact
+              </Link>
+            </li>
+            <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3">
+              <button className="sm:hidden block px-4 py-2 text-sm rounded-full font-medium cursor-pointer tracking-wide text-slate-900 border border-gray-400 bg-transparent hover:bg-gray-50 transition-all">
+                <Link to="/signin">Login</Link>
+              </button>
+            </li>
+            <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3">
+              <button className="sm:hidden block  px-4 py-2 text-sm rounded-full font-medium cursor-pointer tracking-wide text-white border border-blue-600 bg-blue-600 hover:bg-blue-700 transition-all">
+                <Link to="/singup">Sign up</Link>
+              </button>
+            </li>
+          </ul>
+        </div>
+      ) : (
+        <div className="absolute z-999 w-full top-17 translate-x-1150 duration-300 block lg:hidden">
+          <ul className="bg-white w-full ">
+            <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3">
+              <a
+                href="javascript:void(0)"
+                className="hover:text-blue-700 text-blue-700 block font-medium text-[15px]"
+              >
+                Home
+              </a>
+            </li>
+            <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3">
+              <Link
+                to="/todolist"
+                className="hover:text-blue-700 text-slate-900 block font-medium text-[15px]"
+              >
+                Todo List
+              </Link>
+            </li>
+            <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3">
+              <Link
+                to="/todo"
+                className="hover:text-blue-700 text-slate-900 block font-medium text-[15px]"
+              >
+                TO-DO
+              </Link>
+            </li>
+            <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3">
+              <a
+                href="javascript:void(0)"
+                className="hover:text-blue-700 text-slate-900 block font-medium text-[15px]"
+              >
+                Blog
+              </a>
+            </li>
+            <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3">
+              <a
+                href="javascript:void(0)"
+                className="hover:text-blue-700 text-slate-900 block font-medium text-[15px]"
+              >
+                About
+              </a>
+            </li>
+            <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3">
+              <Link
+                to="/contact"
+                className="hover:text-blue-700 text-slate-900 block font-medium text-[15px]"
+              >
+                Contact
+              </Link>
+            </li>
+            <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3">
+              <button className="sm:hidden block px-4 py-2 text-sm rounded-full font-medium cursor-pointer tracking-wide text-slate-900 border border-gray-400 bg-transparent hover:bg-gray-50 transition-all">
+                <Link to="/signin">Login</Link>
+              </button>
+            </li>
+            <li className="max-lg:border-b max-lg:border-gray-300 max-lg:py-3 px-3">
+              <button className="sm:hidden block  px-4 py-2 text-sm rounded-full font-medium cursor-pointer tracking-wide text-white border border-blue-600 bg-blue-600 hover:bg-blue-700 transition-all">
+                <Link to="/singup">Sign up</Link>
+              </button>
+            </li>
+          </ul>
+        </div>
+      )}
     </header>
   );
 }
