9c111a9f05608b65868d6e4cd5512c7771348dd7
proshanto7 <proshantosarker112@gmail.com>
2026-01-17 04:19:27 +0600

    responsive desing for mobile

--- src/component/common/Container.jsx
+++ src/component/common/Container.jsx
@@ -1,14 +1,7 @@
-import React from 'react'
+import React from "react";
 
-function Container({children}) {
-  return (
-    <div className='max-w-300 mx-auto'>
-
-
-{children}
-
-    </div>
-  )
+function Container({ children }) {
+  return <div className="max-w-300 mx-auto px-1.5 lg:px-0">{children}</div>;
 }
 
-export default Container
+export default Container;
